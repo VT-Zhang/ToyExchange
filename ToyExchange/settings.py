a99bb9522e2f8a5deda1f8686209307e4d3e7027
VT-Zhang <jianz@vt.edu>
2017-03-03 12:10:31 -0500

    Changed the Media URL and Media Root

--- ToyExchange/settings.py
+++ ToyExchange/settings.py
@@ -64,6 +64,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media'
             ],
         },
     },
@@ -121,8 +122,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'apps/toys/static/toys/media')
 # MEDIA_ROOT = '/Users/jianzhang/dropbox/toyexchange/ToyExchange/media'
-MEDIA_ROOT = os.path.join(PROJECT_DIR, 'media')
+# MEDIA_ROOT = os.path.join(PROJECT_DIR, 'media')
 
 MEDIA_URL = '/media/'
